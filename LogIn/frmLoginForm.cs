fed080ff314def388f82c3e47c3219316cea9b6f
zidann77 <azeidan061@gmail.com>
2026-02-03 20:30:07 +0300

    Login & Messages

---
+++ LogIn/frmLoginForm.cs
@@ -0,0 +1,114 @@
+﻿using BusinessLogicLayer;
+using Library.Global_Classes;
+using Library.Message;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library.LogIn
+{
+    public partial class frmLoginForm : Form
+    {
+        public frmLoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2ImageButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        string username = "";
+        string password = "";
+        private void frmLoginForm_Load(object sender, EventArgs e)
+        {
+            guna2ToggleSwitch1.CheckedState.FillColor = Color.MediumSeaGreen;
+            guna2ToggleSwitch1.CheckedState.InnerColor = Color.White;
+            guna2ToggleSwitch1.UncheckedState.FillColor = Color.Silver;
+            guna2ToggleSwitch1.UncheckedState.InnerColor = Color.WhiteSmoke;
+            guna2ToggleSwitch1.CheckedState.BorderColor = Color.MediumSeaGreen;
+            guna2ToggleSwitch1.UncheckedState.BorderColor = Color.Gray;
+            guna2ToggleSwitch1.Size = new Size(50, 25);
+            guna2ToggleSwitch1.Cursor = Cursors.Hand;
+
+            txtUserName.Focus();
+            btnLogin.Enabled = false;
+
+           
+
+            if (clsLoginSettings.ReadLoginDataFromRegistry(ref username, ref password))
+            {
+                txtPassword.Text = password;
+                txtUserName.Text = username;
+                btnLogin.Enabled = true;
+                guna2ToggleSwitch1.Checked = true;
+                btnLogin.Focus();
+            }
+
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+            btnLogin.Enabled = !string.IsNullOrEmpty(txtUserName.Text) && !string.IsNullOrEmpty(txtPassword.Text);
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (!this.ValidateChildren())
+            {
+                frmMessage M = new frmMessage("Some Fields Not Valid", frmMessage.enMode.Error);
+                M.ShowDialog();
+            }
+            else
+            {
+                username = txtUserName.Text;
+                password = txtPassword.Text;
+
+                clsGlobal.CurrentUser = clsUser.FindByUserNameAndPassword(username, password);
+
+
+                if (clsGlobal.CurrentUser == null)
+                {
+                    frmMessage M = new frmMessage($"Can't find user with Username: {username} and Password: {password}", frmMessage.enMode.Error);
+                    M.ShowDialog();
+                    clsLogger.LogMessage($"Can't find user with Username: {username} and Password: {password}", System.Diagnostics.EventLogEntryType.Error);
+                    return;
+                }
+
+                if (!clsGlobal.CurrentUser.IsActive)
+                {
+                    frmMessage M = new frmMessage($"User {username} is inactive. Please contact the system administrator.", frmMessage.enMode.Error);
+                    M.ShowDialog();
+                    clsLogger.LogMessage($"Inactive user attempted login: {username}", System.Diagnostics.EventLogEntryType.Warning);
+                    return;
+                }
+
+                if (guna2ToggleSwitch1.Checked)
+                {
+                    username = txtUserName.Text.Trim();
+                    password = txtPassword.Text.Trim();
+                    clsLoginSettings.UpdateLoginDataFromRegistry(username, password);
+                }
+                else
+                {
+                    clsLoginSettings.DeleteLoginDataFromRegistry();
+                }
+
+                clsLogger.LogMessage($"User: {username} Login To The System at {DateTime.Now}", System.Diagnostics.EventLogEntryType.Warning);
+
+                this.Hide();
+                frmMain frm = new frmMain(this);
+                frm.ShowDialog();
+
+            }
+
+        }
+    }
+}
---
+++ Message/frmMessage.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Library.Message
+{
+    partial class frmMessage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.lbtitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOK = new Guna.UI2.WinForms.Guna2Button();
+            this.btnCancel = new Guna.UI2.WinForms.Guna2Button();
+            this.pbImage = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.btnClose = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.guna2Panel1.Controls.Add(this.btnClose);
+            this.guna2Panel1.Controls.Add(this.lbtitle);
+            this.guna2Panel1.Location = new System.Drawing.Point(-18, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(427, 59);
+            this.guna2Panel1.TabIndex = 12;
+            // 
+            // lbtitle
+            // 
+            this.lbtitle.AutoSize = true;
+            this.lbtitle.Font = new System.Drawing.Font("Rockwell", 20F);
+            this.lbtitle.ForeColor = System.Drawing.Color.Linen;
+            this.lbtitle.Location = new System.Drawing.Point(167, 12);
+            this.lbtitle.Name = "lbtitle";
+            this.lbtitle.Size = new System.Drawing.Size(123, 31);
+            this.lbtitle.TabIndex = 10;
+            this.lbtitle.Text = "Message";
+            this.lbtitle.Click += new System.EventHandler(this.lbtitle_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.label1.Location = new System.Drawing.Point(90, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 79);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "label1";
+            // 
+            // btnOK
+            // 
+            this.btnOK.AutoRoundedCorners = true;
+            this.btnOK.BorderRadius = 12;
+            this.btnOK.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnOK.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnOK.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnOK.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnOK.FillColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnOK.Font = new System.Drawing.Font("Rockwell", 11F);
+            this.btnOK.ForeColor = System.Drawing.Color.White;
+            this.btnOK.Location = new System.Drawing.Point(104, 197);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(95, 27);
+            this.btnOK.TabIndex = 15;
+            this.btnOK.Text = "OK";
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.AutoRoundedCorners = true;
+            this.btnCancel.BorderRadius = 12;
+            this.btnCancel.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnCancel.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnCancel.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnCancel.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnCancel.FillColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnCancel.Font = new System.Drawing.Font("Rockwell", 11F);
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(215, 197);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(95, 27);
+            this.btnCancel.TabIndex = 16;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackColor = System.Drawing.Color.Transparent;
+            this.pbImage.BackgroundImage = global::Library.Properties.Resources.error__1_;
+            this.pbImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbImage.FillColor = System.Drawing.Color.Transparent;
+            this.pbImage.ImageRotate = 0F;
+            this.pbImage.Location = new System.Drawing.Point(12, 91);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(72, 64);
+            this.pbImage.TabIndex = 13;
+            this.pbImage.TabStop = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnClose.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btnClose.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btnClose.Image = global::Library.Properties.Resources.icons8_close_48;
+            this.btnClose.ImageOffset = new System.Drawing.Point(0, 0);
+            this.btnClose.ImageRotate = 0F;
+            this.btnClose.Location = new System.Drawing.Point(377, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.PressedState.Image = global::Library.Properties.Resources.icons8_close_48;
+            this.btnClose.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btnClose.Size = new System.Drawing.Size(35, 31);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Click += new System.EventHandler(this.guna2ImageButton1_Click);
+            // 
+            // frmMessage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.ClientSize = new System.Drawing.Size(406, 236);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.guna2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMessage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmMessage";
+            this.Load += new System.EventHandler(this.frmMessage_Load);
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2ImageButton btnClose;
+        private System.Windows.Forms.Label lbtitle;
+        private Guna.UI2.WinForms.Guna2PictureBox pbImage;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Button btnOK;
+        private Guna.UI2.WinForms.Guna2Button btnCancel;
+    }
+}
